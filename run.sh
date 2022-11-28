5b1d890ac7e44042bf2ec1538173639b4e9aaa1d
Laura Paulsen <202005791@post.au.dk>
2022-11-28 21:46:53 +0100

    init bash script for replication and requirements

---
+++ run.sh
@@ -0,0 +1,31 @@
+# create environment
+python3.8 -m venv .venv
+
+# activate environment
+source .venv/bin/activate
+
+# get newsFluxus package from Centre for humanities computing
+git clone https://github.com/centre-for-humanities-computing/newsFluxus.git
+
+# install dependencies
+pip install -r newsFluxus/requirements.txt
+pip install -r requirements.txt
+
+# create json file with information about the parties
+python create_party_info.py
+
+# scrape twitter
+python src/scrape_twitter.py
+
+# clean data
+python src/clean_data.py
+
+# emotion classification
+python src/emotion_classification.py
+
+# summarize emotions script from emoDynamics 
+python src/summarize_emotions.py --filepath data/preprocessed/dk_pol_data_emotions.csv --output_name emotions_summarized --emotion_col emotion --time_col date_created 
+
+# use newsFluxus to get novelty, resonnance and transience
+python src/emotion_fluxus.py --filenames emotions_summarized_date
+python src/emotion_fluxus.py --filenames emotions_summarized_date_hour
